d7beee3df9990f5618eebbfa04b8032b1f55ada7
Jan Kosinski <jan.kosinski@embl.de>
2021-11-11 14:56:23 +0100

    Add ranges to run_AF_complex.sh

--- run_AF_complex.sh
+++ run_AF_complex.sh
@@ -28,19 +28,20 @@ echo $max_recycles
 echo $homooligomer
 echo $msa_method
 echo $pair_mode
+echo $ranges
 
-module load AlphaFold/2.0.0-20210910_02_1d43aaf-fosscuda-2020b
+module load AlphaFold/2.1.1-fosscuda-2020b
 module load matplotlib/3.3.3-fosscuda-2020b IPython/7.18.1-GCCcore-10.2.0 tqdm/4.60.0-GCCcore-10.2.0 #required modules compatible with fosscuda-2020b
 
 if [[ "${fasta}" ]]; then
     time PYTHONPATH=/g/kosinski/kosinski/devel/alphafold:$PYTHONPATH \
     TF_FORCE_UNIFIED_MEMORY='1' XLA_PYTHON_CLIENT_MEM_FRACTION='4.0' \
     python /g/kosinski/kosinski/devel/alphafold/sokrypton_alphafold2_advanced.py \
-        --fasta $fasta --jobprefix $jobprefix --max_recycles $max_recycles --homooligomer $homooligomer --msa_method $msa_method --pair_mode $pair_mode
+        --fasta $fasta --jobprefix $jobprefix --max_recycles $max_recycles --homooligomer $homooligomer --msa_method $msa_method --pair_mode $pair_mode --ranges $ranges
 elif [[ "${fastas}" ]]; then
     time PYTHONPATH=/g/kosinski/kosinski/devel/alphafold:$PYTHONPATH \
     TF_FORCE_UNIFIED_MEMORY='1' XLA_PYTHON_CLIENT_MEM_FRACTION='4.0' \
     python /g/kosinski/kosinski/devel/alphafold/sokrypton_alphafold2_advanced.py \
-        --fastas $fastas --jobprefix $jobprefix --max_recycles $max_recycles --homooligomer $homooligomer --msa_method $msa_method --pair_mode $pair_mode
+        --fastas $fastas --jobprefix $jobprefix --max_recycles $max_recycles --homooligomer $homooligomer --msa_method $msa_method --pair_mode $pair_mode --ranges $ranges
 fi
 
